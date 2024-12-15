51fd5e616454ccd92219d154f8fbe72d66941e4c
yuki <yuki.laurentiu@gmail.com>
2024-12-15 17:40:55 +0900

    rewrite app

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Card from './components/Card.jsx'
-import CreateTodo from './components/CreateTodo.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CreateTodo />
+    <App />
   </StrictMode>,
 )
