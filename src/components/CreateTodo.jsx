09ab976b3f34a1b3db220a2fe1e9aa887926a7e9
yuki <yuki.laurentiu@gmail.com>
2024-12-15 10:52:52 +0900

    added two elements in a JSXfile

---
+++ src/components/CreateTodo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CreateTodo = () => {
+  return (
+  <>
+    <h1>Create new Todo from here</h1>
+    <button>Add Task</button>
+  </>
+  );
+};
+
+export default CreateTodo
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Card from './components/Card.jsx'
+import CreateTodo from './components/CreateTodo.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Card />
+    <CreateTodo />
   </StrictMode>,
 )
