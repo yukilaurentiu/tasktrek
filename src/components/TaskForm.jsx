0fcb021a42be50f793fd9e7bfda9029e980b592c
yuki <yuki.laurentiu@gmail.com>
2024-12-18 16:28:03 +0900

    added task-form styling

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
+import './TaskForm.css';
 
 const TaskForm = () => {
   return (
     <header className='app_header'>
       <form>
-        <input type='text' clasName='text_input' placeholder='Enter your task'/>
-        <div clasName='task_form_bottom_line'></div>
-        <button clasNeme='tag'>HTML</button>
-        <button clasNeme='tag'>CSS</button>
-        <button clasNeme='tag'>JavaScript</button>
-        <button clasNeme='tag'>React</button>
-        
-        <select clasName='task_status'>
-          <option value='todo'>Todo</option>
-          <option value='doing'>Doing</option>
-          <option value='done'>Done</option>
-        </select>
-        <button type='submit' className='task_submit'>+ Add task</button>
+        <input type='text' className='task_input' placeholder='Enter your task'/>
+        <div className='task_form_bottom_line'>
+          <div>
+            <button className='tag'>HTML</button>
+            <button className='tag'>CSS</button>
+            <button className='tag'>JavaScript</button>
+            <button className='tag'>React</button>
+          </div>
+
+          <div>
+            <select className='task_status'>
+              <option value='todo'>Todo</option>
+              <option value='doing'>Doing</option>
+              <option value='done'>Done</option>
+            </select>
+            <button type='submit' className='task_submit'>+ Add task</button>
+          </div>
+        </div>
       </form>
     </header>
     
